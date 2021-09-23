6a80b0db690a423f2db869fe8e96a0a051a49ad2
resyfer <palsaurav.2020@gmail.com>
2021-09-24 00:53:25 +0530

    Updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,35 @@
 //* React
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-const Home = (props) => {
-  //* Set Title
-  useEffect(() => {
-    document.title = props.title;
-  }, [props.title]);
+//! Temporary
+import { Link } from 'react-router-dom';
 
-  return (
-    <main className="home">
-      Home
-      <br />
-      Page
-    </main>
-  );
+import Button from '../components/Button';
+
+import './css/Home.scss';
+
+const Home = props => {
+	//* Set Title
+	useEffect(() => {
+		document.title = props.title;
+	}, [props.title]);
+
+	return (
+		<div className='home'>
+			<div className='logo'>
+				<img src='/img/logo.png' alt='Errorless' />
+			</div>
+			<div className='title'>
+				<h1>Errorless</h1>
+				<h2>Helping breathe in happiness, not COVID-19</h2>
+			</div>
+
+			{/* !Temporary */}
+			<Link to='institute/1234'>Home</Link>
+
+			<Button name='Login / Sign Up' link='/auth' />
+		</div>
+	);
 };
 
 export default Home;
