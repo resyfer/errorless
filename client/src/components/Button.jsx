37167c557ec57e2c965d51275c3303881f6e2c88
resyfer <palsaurav.2020@gmail.com>
2021-09-23 16:46:35 +0530

    button, primitive input

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,19 @@
 //* React
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
+//* CSS
+import './css/Button.scss';
+
 const Button = props => {
+	const link = useRef();
+
 	return (
-		<Link className='button' to={props.link}>
-			{props.name}
-		</Link>
+		<div className='btn' onClick={() => link.current.click()}>
+			<Link to={props.link} ref={link} className='btn-link'>
+				{props.name}
+			</Link>
+		</div>
 	);
 };
 
