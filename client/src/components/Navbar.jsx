12d3567ce6f75be93ad03c73ac5a921732963c48
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-09-24 10:44:33 +0530

    merge conflicts resolved

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,43 +24,6 @@ const Navbar = (props) => {
   const { loggedIn, setLoggedIn, user, setUser } = useContext(UserContext);
   const history = useHistory();
 
-<<<<<<< HEAD
-	const handleLogout = () => {
-		if (loggedIn) {
-			Cookies.remove('user');
-			Cookies.remove('jwt');
-			setLoggedIn(false);
-			setUser(null);
-			history.push('/');
-		}
-	};
-
-	return (
-		<div className='navbar'>
-			<Link to={loggedIn ? `/user/${user._id}` : '/'} className='logo-link'>
-				<img src='/img/logo.png' className='logo' alt='CoLive-21' />
-			</Link>
-			{loggedIn && (
-				<ul className='nav-items-ctnr'>
-					<li>
-						<Link to='/institute'>Institute</Link>
-					</li>
-					<li>
-						<Link to='/profile'>Profile</Link>
-					</li>
-					<li>
-						<Link to='/team'>About Us</Link>
-					</li>
-				</ul>
-			)}
-			{loggedIn ? (
-				<NoLinkButton name='Logout' onClick={handleLogout} />
-			) : (
-				<Button name='Login / Sign Up' link='/auth' />
-			)}
-		</div>
-	);
-=======
   const handleLogout = () => {
     if (loggedIn) {
       Cookies.remove("user");
@@ -96,7 +59,6 @@ const Navbar = (props) => {
       )}
     </div>
   );
->>>>>>> 7aa4f73bea8c4fc6fd7cf70ef9827c9edf7b17b1
 };
 
 export default Navbar;
