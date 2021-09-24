5f3fa58d1f9fff571ac72d833765beb6f3cd92cb
Pratik Majumdar(codadept) <info.pratikmajumdar@gmail.com>
2021-09-24 15:21:55 +0530

    made profile component dynamic

--- client/src/pages/Institute.jsx
+++ client/src/pages/Institute.jsx
@@ -1,150 +1,154 @@
 //* React
-import { useState, useEffect, useContext } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useEffect, useContext } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import axios from "axios";
 
-import UserContext from '../context/UserContext';
+import UserContext from "../context/UserContext";
 
-import vaccinationStatus from '../context/vaccinationStatus';
+import vaccinationStatus from "../context/vaccinationStatus";
 
-import { PieChart } from 'react-minimal-pie-chart';
+import { PieChart } from "react-minimal-pie-chart";
 
 //* CSS
-import './css/Institute.scss';
+import "./css/Institute.scss";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const Institute = () => {
-	const [orgData, setOrgData] = useState(null);
-	const [usersData, setUsersData] = useState(null);
+  const [orgData, setOrgData] = useState(null);
+  const [usersData, setUsersData] = useState(null);
 
-	const history = useHistory();
-	const params = useParams();
+  const history = useHistory();
+  const params = useParams();
 
-	const { loggedIn } = useContext(UserContext);
+  const { loggedIn } = useContext(UserContext);
 
-	//* Set Title
-	useEffect(() => {
-		if (orgData) document.title = orgData.name + ' | CoLive-21';
-		else document.title = 'Institute | CoLive-21';
-	}, [orgData]);
+  //* Set Title
+  useEffect(() => {
+    if (orgData) document.title = orgData.name + " | CoLive-21";
+    else document.title = "Institute | CoLive-21";
+  }, [orgData]);
 
-	useEffect(() => {
-		axios
-			.get(`${apiUrl}/institute/${params.id}`)
-			.then(res => res.data)
-			.then(data => setOrgData(data.org))
-			.then(
-				axios(`${apiUrl}/users`)
-					.then(res => res.data)
-					.then(data => setUsersData(data.users))
-			);
-	}, [params.id]);
+  useEffect(() => {
+    axios
+      .get(`${apiUrl}/institute/${params.id}`)
+      .then((res) => res.data)
+      .then((data) => setOrgData(data.org))
+      .then(
+        axios(`${apiUrl}/users`)
+          .then((res) => res.data)
+          .then((data) => setUsersData(data.users))
+      );
+  }, [params.id]);
 
-	useEffect(() => {
-		if (!loggedIn) history.push('/');
-	});
+  useEffect(() => {
+    if (!loggedIn) history.push("/");
+  });
 
-	return (
-		<main className='insti'>
-			<h3 className='insti-name'>{orgData && orgData.name}</h3>
-			<div className='insti-info'>
-				<div className='info-left'>
-					<img src={orgData && orgData.img} alt={orgData && orgData.name} />
-				</div>
-				<div className='info-right'>
-					<div className='description'>{orgData && orgData.description}</div>
-					<div className='members-count'>
-						Members: {orgData && orgData.crew.length}
-					</div>
-				</div>
-			</div>
+  return (
+    <main className="insti">
+      <h3 className="insti-name">{orgData && orgData.name}</h3>
+      <div className="insti-info">
+        <div className="info-left">
+          <img src={orgData && orgData.img} alt={orgData && orgData.name} />
+        </div>
+        <div className="info-right">
+          <div className="description">{orgData && orgData.description}</div>
+          <div className="members-count">
+            Members: {orgData && orgData.crew.length}
+          </div>
+        </div>
+      </div>
 
-			<div className='heading'>Stats</div>
-			{orgData && (
-				<div className='chart'>
-					<div className='pie-chart'>
-						<PieChart
-							data={[
-								{
-									title: vaccinationStatus[0],
-									value: orgData.status[0],
-									color: 'var(--theme-2-100)',
-								},
-								{
-									title: vaccinationStatus[1],
-									value: orgData.status[1],
-									color: 'var(--theme-3-100)',
-								},
-								{
-									title: vaccinationStatus[2],
-									value: orgData.status[2],
-									color: 'var(--theme-4-100)',
-								},
-							]}
-							animate={true}
-							startAngle={-30}
-						/>
-					</div>
-					<div className='legend'>
-						<div className='legend-itm'>
-							<div
-								className='legend-color'
-								style={{ backgroundColor: 'var(--theme-4-100)' }}></div>
-							<div className='legend-label'>
-								{vaccinationStatus[2]} - {orgData.status[2]}
-							</div>
-						</div>
-						<div className='legend-itm'>
-							<div
-								className='legend-color'
-								style={{ backgroundColor: 'var(--theme-3-100)' }}></div>
-							<div className='legend-label'>
-								{vaccinationStatus[1]} - {orgData.status[1]}
-							</div>
-						</div>
-						<div className='legend-itm'>
-							<div
-								className='legend-color'
-								style={{ backgroundColor: 'var(--theme-2-100)' }}></div>
-							<div className='legend-label'>
-								{vaccinationStatus[0]} - {orgData.status[0]}
-							</div>
-						</div>
-					</div>
-				</div>
-			)}
+      <div className="heading">Stats</div>
+      {orgData && (
+        <div className="chart">
+          <div className="pie-chart">
+            <PieChart
+              data={[
+                {
+                  title: vaccinationStatus[0],
+                  value: orgData.status[0],
+                  color: "var(--theme-2-100)",
+                },
+                {
+                  title: vaccinationStatus[1],
+                  value: orgData.status[1],
+                  color: "var(--theme-3-100)",
+                },
+                {
+                  title: vaccinationStatus[2],
+                  value: orgData.status[2],
+                  color: "var(--theme-4-100)",
+                },
+              ]}
+              animate={true}
+              startAngle={-30}
+            />
+          </div>
+          <div className="legend">
+            <div className="legend-itm">
+              <div
+                className="legend-color"
+                style={{ backgroundColor: "var(--theme-4-100)" }}
+              ></div>
+              <div className="legend-label">
+                {vaccinationStatus[2]} - {orgData.status[2]}
+              </div>
+            </div>
+            <div className="legend-itm">
+              <div
+                className="legend-color"
+                style={{ backgroundColor: "var(--theme-3-100)" }}
+              ></div>
+              <div className="legend-label">
+                {vaccinationStatus[1]} - {orgData.status[1]}
+              </div>
+            </div>
+            <div className="legend-itm">
+              <div
+                className="legend-color"
+                style={{ backgroundColor: "var(--theme-2-100)" }}
+              ></div>
+              <div className="legend-label">
+                {vaccinationStatus[0]} - {orgData.status[0]}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
-			<div className='heading'>Members</div>
-			<div className='members'>
-				{orgData &&
-					usersData &&
-					usersData.map(user => {
-						if (user.organisation.orgId === orgData._id)
-							return (
-								<div
-									key={user.id}
-									className='member-card'
-									onClick={() => history.push(`/user/${user.id}`)}>
-									<div className='member-pic'>
-										<img src={user.img} alt={user.name} />
-									</div>
-									<div className='member-info'>
-										<div className='user-name'>{user.name}</div>
-										<div className='user-designation'>
-											{user.organisation.designation}
-										</div>
-										<div className='user-vac-status'>
-											{vaccinationStatus[user.vaccinationStatus]}
-										</div>
-									</div>
-								</div>
-							);
-						else return false;
-					})}
-			</div>
-		</main>
-	);
+      <div className="heading">Members</div>
+      <div className="members">
+        {orgData &&
+          usersData &&
+          usersData.map((user) => {
+            if (user.organisation.orgId === orgData._id)
+              return (
+                <div
+                  key={user.id}
+                  className="member-card"
+                  onClick={() => history.push(`/user/${user._id}`)}
+                >
+                  <div className="member-pic">
+                    <img src={user.img} alt={user.name} />
+                  </div>
+                  <div className="member-info">
+                    <div className="user-name">{user.name}</div>
+                    <div className="user-designation">
+                      {user.organisation.designation}
+                    </div>
+                    <div className="user-vac-status">
+                      {vaccinationStatus[user.vaccinationStatus]}
+                    </div>
+                  </div>
+                </div>
+              );
+            else return false;
+          })}
+      </div>
+    </main>
+  );
 };
 
 export default Institute;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,25 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 import UserContext from "../context/UserContext";
 import vaccinationStatus from "../context/vaccinationStatus";
 
 import NoLinkButton from "../components/NoLinkButton";
 
+import axios from "axios";
+
 import "./css/Profile.scss";
 
 const Profile = (props) => {
   const { user, loggedIn } = useContext(UserContext);
+  const [profileUser, setProfileUser] = useState();
+
+  const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
-    document.title = user?.name + " | CoLive-21";
-  }, [user?.name]);
+    document.title = profileUser?.name + " | CoLive-21";
+  }, [profileUser?.name]);
 
   // useEffect(() => {
   //   if (!loggedIn) {
@@ -20,53 +27,70 @@ const Profile = (props) => {
   //   }
   // }, [loggedIn]);
 
+  useEffect(() => {
+    const userId = window.location.pathname.split("/user/")[1];
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/user/${userId}`)
+      .then((res) => {
+        if (!res.data.success) {
+          history.push(`/institute/${user.organisation.orgId}`);
+        } else {
+          setProfileUser(res.data.user);
+        }
+      })
+      .catch();
+  }, [location]);
+
   console.log(loggedIn);
 
   return (
     <main className="profile">
-      {user && (
+      {profileUser && (
         <div className="profileContainer">
           <div className="profileLeft">
             <div className="profileImg">
               <img
                 src={
-                  user?.photo
-                    ? user?.photo
+                  profileUser?.photo
+                    ? profileUser?.photo
                     : "https://lh3.googleusercontent.com/pw/AM-JKLUYXZMkLSuQHENLROTjKQLYnzNZt7Q4rkmdtAyBtGJfNW32w1k6jaTC2Oju7xHORmlTRTOiyyuhewCM10hpS9Rt4BQeIk3WJ7MHzlFpozHx3KXGdAHHV9vEq2XmeOERSSiiqbziNAVGRHI7lVPW78G4=s225-no"
                 }
-                alt={user?.name}
+                alt={profileUser?.name}
               />
             </div>
             <div className="profileDetails">
-              <p className="profileUserName">{user?.name}</p>
+              <p className="profileUserName">{profileUser?.name}</p>
               <p className="profileUserEmail">
-                <i className="fas fa-envelope"></i> {user?.email}
+                <i className="fas fa-envelope"></i> {profileUser?.email}
               </p>
               <p className="profileUserPhone">
                 <i className="fas fa-phone"></i>{" "}
-                {user.phoneNo ? user.phoneNo : "Not added"}
+                {profileUser.phoneNo ? profileUser.phoneNo : "Not added"}
               </p>
               <p className="profileUserDesg">
-                {user?.organisation.designation}
+                {profileUser?.organisation?.designation}
               </p>
               <p className="profileUseOrg">
-                <i className="fas fa-university"></i> {user?.organisation?.name}
+                <i className="fas fa-university"></i>{" "}
+                {profileUser?.organisation?.name}
               </p>
               <p className="profileUserVaccStatus">
-                {user.vaccinationStatus !== null &&
-                  vaccinationStatus[user.vaccinationStatus]}
+                {profileUser.vaccinationStatus !== null &&
+                  vaccinationStatus[profileUser.vaccinationStatus]}
               </p>
-              <NoLinkButton
-                style={{ padding: "1vh 2vh", margin: "1rem auto" }}
-                name="Download your QR Code"
-              />
+              {profileUser._id === user._id && (
+                <NoLinkButton
+                  style={{ padding: "1vh 2vh", margin: "1rem auto" }}
+                  name="Download your QR Code"
+                />
+              )}
             </div>
           </div>
           <div className="profileRight">
             <p className="profileUserHistoryHeading">History of the user</p>
             <div className="profileUserHistoryOverflow">
-              {user.history.length > 0 &&
-                user.history.map((h, i) => (
+              {profileUser.history.length > 0 &&
+                profileUser.history.map((h, i) => (
                   <div className="profileUserHistory" key={i}>
                     <span>
                       <i className="fas fa-notes-medical"></i> {h.event}
